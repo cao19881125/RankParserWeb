1f5919b48bfbe9ae5a429e035b629ae968a0e20c
Yuntao Cao <yuntao.cao@nocsys.com.cn>
2020-06-18 14:36:40 +0800

    first commit

---
+++ js/common.js
@@ -0,0 +1,27 @@
+function fill_table(data,tablename){
+        var result_array = new Array()
+
+        var num = 1
+        for(var pd in data){
+            var trdata = data[pd]
+            var per_data_array = new Array()
+            per_data_array.push(num)
+            per_data_array.push(trdata.name)
+            per_data_array.push(trdata.score)
+            per_data_array.push(trdata.rank)
+            //per_data_array.push(trdata.url)
+            per_data_array.push("<a class=\"text-primary\" href=\"" + trdata.url + "\">查看</a>")
+            result_array.push(per_data_array)
+            num = num + 1
+        }
+
+//        console.log(result_array)
+        mytable = $(tablename).DataTable( {
+            data:result_array,
+            drawCallback: function(){
+                $('.paginate_button', this.api().table().container())
+                    .on('click', table_change());
+            }
+        } );
+
+}
---
+++ js/warrior.js
@@ -0,0 +1,89 @@
+
+function send_request(url,type,data,success_call_back,error_call_back) {
+    $.ajax({
+        //url: 'http://192.168.184.128:8080/port_mapper',
+        url: url,
+        type: type, /* or type:"GET" or type:"PUT" */
+        data:data,
+        success: function (data, textStatus, response) {
+            //console.log(data)
+            success_call_back(data,textStatus, response)
+
+        },
+        error: function (result) {
+            console.log(result);
+        }
+    });
+}
+
+/* 初始化client列表
+
+------------*/
+
+
+function table_change() {
+    //alert('next');
+    var $tt = $('#checkAll')
+    $tt.prop('checked',false)
+
+
+    var $tbr = $('table tbody tr');
+    $tbr.find('input').prop('checked',false)
+
+}
+
+
+$(document).ready(function() {
+
+    function handle_response_data(data) {
+        //console.log(data["Mage"]["MoltenCore"])
+
+        $("#UpdateTimeDiv").text("更新时间: " + data["UpdateTime"])
+
+        var class_id = $("#ClassID").text()
+
+        fill_table(data[class_id]["MoltenCore"],'#myTable')
+
+        fill_table(data[class_id]["BlackwingLair"],'#myTable2')
+
+    }
+
+    function refresh_data() {
+        send_request('http://127.0.0.1:8080/rank_data',"GET",null,handle_response_data)
+    }
+
+    function refresh_table() {
+
+        mytable.clear()
+        mytable.destroy()
+        refresh_data()
+    }
+
+    $(function () {
+
+        refresh_data()
+
+    });
+
+
+});
+
+$(function () {
+    function initTableCheckbox(){
+        var $thr = $('table thead tr')
+
+        var $checkAll = $thr.find('input');
+        $checkAll.click(function(event){
+            var $tbr = $('table tbody tr');
+            $tbr.find('input').prop('checked',$(this).prop('checked'));
+            event.stopPropagation();
+
+        });
+
+
+    }
+
+    initTableCheckbox()
+});
+
+
