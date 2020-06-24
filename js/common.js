38404f02e8cd7b30d8e7f02615979f39ef6f08e5
Yuntao Cao <yuntao.cao@nocsys.com.cn>
2020-06-24 18:03:03 +0800

    add target property

--- js/common.js
+++ js/common.js
@@ -6,11 +6,11 @@ function fill_table(data,tablename){
             var trdata = data[pd]
             var per_data_array = new Array()
             per_data_array.push(num)
-            per_data_array.push("<a class=\"text-primary\" href=\"" + trdata.url + "\">" + trdata.name + "</a>")
+            per_data_array.push("<a class=\"text-primary\" target=\"_blank\" href=\"" + trdata.url + "\">" + trdata.name + "</a>")
             //per_data_array.push(trdata.name)
             per_data_array.push(trdata.score)
             per_data_array.push(trdata.rank)
-            per_data_array.push("<a class=\"text-primary\" href=\"" + trdata.serverRankHref + "\">" +  trdata.serverRank.toString() + "（" + trdata.serverRankDps.toString() + ")</a>")
+            per_data_array.push("<a class=\"text-primary\" target=\"_blank\" href=\"" + trdata.serverRankHref + "\">" +  trdata.serverRank.toString() + "（" + trdata.serverRankDps.toString() + ")</a>")
             //per_data_array.push(trdata.url)
             //per_data_array.push("<a class=\"text-primary\" href=\"" + trdata.url + "\">查看</a>")
             result_array.push(per_data_array)
